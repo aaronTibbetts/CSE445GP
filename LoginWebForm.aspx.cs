ed64b7c765f594e4edac56e0085694e7ea00e804
TherenWhite <124221153+TherenWhite@users.noreply.github.com>
2025-04-27 00:28:19 -0700

    Add files via upload

--- LoginWebForm.aspx.cs
+++ LoginWebForm.aspx.cs
@@ -3,43 +3,130 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.IO;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Serialization;
 using myLib;
+
 namespace WebApplication1
 {
-	public partial class LoginWebForm : System.Web.UI.Page
-	{
-		protected void Page_Load(object sender, EventArgs e)
-		{
+    public partial class LoginWebForm : System.Web.UI.Page
+    {
+        private string location = Path.Combine(HttpRuntime.AppDomainAppPath, @"App_Data\doc.xml");
 
-		}
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string directory = Path.GetDirectoryName(location);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
 
         protected void CreateAccount_Click(object sender, EventArgs e)
         {
-			//implement captcha here 
-			Response.Redirect("CreateWebForm.aspx");
+            Response.Redirect("CreateWebForm.aspx");
         }
 
         protected void LoginBttn_Click(object sender, EventArgs e)
         {
-            string psswrd = PasswordTxtBx.Text;
-            string encrypt;
-            string decrypt;
+            string username = UsernameTxtBx.Text;
+            string password = PasswordTxtBx.Text;
 
-
-
-            using (SymmetricAlgorithm des = DES.Create())
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
             {
-                des.GenerateKey();
-                des.GenerateIV();
-
-                encrypt = Class1.EncryptString(psswrd, des.Key, des.IV);
-                decrypt = Class1.DecryptString(encrypt, des.Key, des.IV);
+                Label1.Text = "Enter both username and password";
+                Label1.Visible = true;
+                return;
             }
-            Label1.Text = encrypt;
-            Label2.Text = decrypt;
+            if (VerifyUser(username, password))
+            {
+                Session["LoggedIn"] = true;
+                Session["Username"] = username;
+
+                Response.Redirect("DEFAULT PAGE"); //default page redirection needed
+            }
+            else
+            {
+                Label1.Text = "Username and/or password are incorrect";
+                Label1.Visible = true;
+            }
+        }
+
+        private bool VerifyUser(string username, string password)
+        {
+            List<User> allUsers = new List<User>();
+            
+            if (File.Exists(location))
+            {
+                XmlSerializer des = new XmlSerializer(typeof(List<User>));
+                using (FileStream fileStream = new FileStream(location, FileMode.Open))
+                {
+                    try
+                    {
+                        allUsers = (List<User>)des.Deserialize(fileStream);
+                    }
+                    catch (Exception ex)
+                    {
+                        Label1.Text = "Error reading user data";
+                        Label1.Visible = true;
+                        return false;
+                    }
+                }
+                User foundUser = allUsers.FirstOrDefault(u => u.UserName == username);
+                
+                if (foundUser != null)
+                {
+
+                    try
+                    {
+                        string decryptedPassword = Class1.DecryptString(foundUser.PassWord, foundUser.Key, foundUser.IV);
+                        return decryptedPassword == password;
+                    }
+                    catch (Exception ex)
+                    {
+                        Label1.Text = "Error verifying password";
+                        Label1.Visible = true;
+                        return false;
+                    }
+                }
+            }
+            
+            return false;
+        }
+    }
+
+    public class User
+    {
+        string userName;
+        string passWord;
+        byte[] iv;
+        byte[] key;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        public string PassWord
+        {
+            get { return passWord; }
+            set { passWord = value; }
+        }
+
+        public byte[] IV
+        {
+            get { return iv; }
+            set { iv = value; }
+        }
+
+        public byte[] Key
+        {
+            get { return key; }
+            set { key = value; }
         }
     }
 }
